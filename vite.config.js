bf8895965f5236360e81fb0af080a386c6a69143
Em1lioV <emilio.villagrawt@gmail.com>
2023-10-02 22:59:56 -0300

    deploy 1.0

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  bese: '/Weather-app/',
+  base: '/Weather-app/',
   plugins: [
     vue(),
   ],
